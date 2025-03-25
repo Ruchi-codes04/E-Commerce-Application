09670bbd66681f6f413fc72e695eda99b59a3357
Ruchi-codes04 <p.ruchika142003@gmail.com>
2025-03-25 22:10:06 +0530

    Navbar-Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center w-full justify-between  font-medium"> {/* py-[20px] */}
       <div>
@@ -23,7 +24,7 @@ const Navbar = () => {
             className="hover:text-[#111827] text-inherit no-underline"
           >
             <p>HOME</p>
-            {/* <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/> */}
+            <hr className='w-2/4 border-none h-[1.6px] bg-gray-700 hidden'/>
           </NavLink>
         </li>
 
@@ -33,7 +34,7 @@ const Navbar = () => {
             className="hover:text-[#111827] text-inherit no-underline"
           >
             <p> COLLECTIONS</p>
-            {/* <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/> */}
+            <hr className='w-2/4 border-none h-[1.6px] bg-gray-700 hidden'/>
           </NavLink>
         </li>
 
@@ -43,7 +44,7 @@ const Navbar = () => {
             className="hover:text-[#111827] text-inherit no-underline"
           >
             <p>ABOUT</p>
-            {/* <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/> */}
+            <hr className='w-2/4 border-none h-[1.6px] bg-gray-700 hidden'/>
           </NavLink>
         </li>
 
@@ -53,7 +54,7 @@ const Navbar = () => {
             className="hover:text-[#111827] text-inherit no-underline"
           >
             <p>CONTACT</p>
-            {/* <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/> */}
+            <hr className='w-2/4 border-none h-[1.6px] bg-gray-700 hidden'/>
           </NavLink>
         </li>
       </ul>
@@ -83,10 +84,29 @@ const Navbar = () => {
            <p className='absolute right-[-5px] bottom-[-5px] w-[16px] text-center leading-[16px] bg-[#000] text-[#fff] aspect-square rounded-full text-[8px]'>10</p>
 
         </Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} alt="menu-icon" className='w-5 cursor-pointer sm:hidden'/>
+       </div>
+
+       {/* Sidebar menu for small screens */}
+       <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+             <div className="flex flex-col text-gray-600">
+              <div onClick={()=>setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
+                <img src={assets.dropdown_icon} alt="dropdown-icon" className="h-4 rotate-180" />
+                <p>Back</p>
+              </div>
+
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collections'>COLLECTIONS</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+              <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+
+
+             </div>
+
+       </div>
 
 
 
-      </div>
     </div>
   );
 };
